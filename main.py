41b6cc71e436c824d2aa0cc0fde991a3b587f459
krdelva <kevin.r.delva@gmail.com>
2025-03-15 00:34:00 -0400

    remove printing screen sizes, modify pygame.Surface.fill to screen.fill with the color black

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from constants import *
 
 def main():
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -13,7 +11,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, color="black")
+        screen.fill("black")
         pygame.display.flip()
 
 if __name__ == "__main__":
