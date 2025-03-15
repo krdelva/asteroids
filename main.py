77699cf59f57cf7153b6ae15e60e7c53a45ac649
krdelva <kevin.r.delva@gmail.com>
2025-03-15 00:42:40 -0400

    modify to include division of 1000 in a single line

--- main.py
+++ main.py
@@ -15,8 +15,9 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-        dt = clock.tick(60)
-        dt /= 1000
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
