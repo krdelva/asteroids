11ff5ec4c8f08c7d1686ad918defb5485901a13b
krdelva <kevin.r.delva@gmail.com>
2025-03-15 00:41:10 -0400

    add pygame.time.Clock object instance and create dt var

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -13,6 +15,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)
+        dt /= 1000
 
 if __name__ == "__main__":
     main()
